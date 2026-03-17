e345f370a1baf69f47b15948542a4dade2c1d1e7
67160173-panutsaya <67160173@go.buu.ac.th>
2026-03-17 20:39:49 +0700

    add large file with LFS

--- app.py
+++ app.py
@@ -1,23 +1,49 @@
 import streamlit as st
-import joblib
 import pandas as pd
+import joblib
 
+st.set_page_config(
+    page_title="Sales Prediction App",
+    page_icon="📊",
+    layout="centered"
+)
+
+st.markdown("""
+<style>
+.stButton>button {
+    background-color: #03a9f4;
+    color: white;
+    border-radius: 10px;
+    height: 3em;
+    width: 100%;
+}
+</style>
+""", unsafe_allow_html=True)
+
+st.title("📊 Sales Prediction Dashboard")
+st.markdown("ทำนายยอดขายจาก ราคา เวลา และประเทศ 🌍")
+
+# โหลดโมเดล + encoder
 model = joblib.load("sales_model.pkl")
+le = joblib.load("encoder.pkl")
+print(model.feature_names_in_)
 
-st.title("Online Retail Sales Prediction")
 
-quantity = st.number_input("Quantity")
-UnitPrice = st.number_input("Unit Price")
-country = st.number_input("Country Code")
+# รับค่าจากผู้ใช้
+unit_price = st.number_input("Unit Price", 0.0, 1000.0)
+hour = st.slider("Hour", 0, 23)
+month = st.slider("Month", 1, 12)
 
+country_name = st.selectbox("Country", le.classes_)
+country = le.transform([country_name])[0]
+
+
+
+# 👇 แล้วค่อยเอาไปใช้
 if st.button("Predict"):
-
-    input_data = pd.DataFrame({
-    "Quantity":[quantity],
-    "UnitPrice":[UnitPrice],
-    "Country":[country]
-})
+    input_data = pd.DataFrame([[1, unit_price, hour, month]],
+                          columns=['Quantity', 'UnitPrice', 'Hour', 'Month'])
 
     prediction = model.predict(input_data)
 
-    st.success(f"Predicted Sales: {prediction[0]}")
+    st.success(f"Predicted Quantity: {prediction[0]}")
